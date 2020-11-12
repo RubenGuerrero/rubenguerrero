1c815a93a478b2b0abb0c1c0cc1d681ec5def8cc
Rubén Guerrero <yo@rubenguerrero.com>
2020-11-12 19:04:24 +0100

    first version

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-gray-300">
       <Head>
-        <title>Create Next App</title>
+        <title>Frontend Engineer - Rubén Guerrero</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Start</main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className="p-5 bg-white rounded-lg md:flex md:items-center">
+        <img
+          className="w-24 h-24 mx-auto rounded-full md:mx-0"
+          src="https://unavatar.now.sh/twitter/ruben_guerrero"
+          alt=""
+        />
+        <div className="mt-5 text-center md:text-left md:mt-0 md:ml-5">
+          <h2 className="text-lg font-bold text-gray-900">Rubén Guerrero</h2>
+          <div className="text-purple-500">Frontend Engineer</div>
+          <div className="text-gray-600">www.rubenguerrero.com</div>
+        </div>
+      </div>
     </div>
   );
 }
