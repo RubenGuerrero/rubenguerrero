2c31111af07dbc1275347014f8e9cb18f92a2714
Rubén Guerrero <yo@rubenguerrero.com>
2020-11-12 18:11:49 +0100

    feat: initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any,
+};
+
+export default MyApp;
