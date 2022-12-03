80096d8f3ceafdf608dfc7ed091696e8f09f19c2
RubenGuerrero <ruben.guerrero@boopos.com>
2022-12-03 11:55:39 +0100

    feat: remove GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
+import { Inter } from '@next/font/google';
 
-import * as gtag from 'lib/gtag';
+const inter = Inter({ subsets: ['latin'] });
 
 import 'styles/globals.css';
 import { AppProps } from 'next/app';
@@ -11,26 +10,19 @@ const description =
   'Software Engineer with +9 years of experience in web development. Worked on multiple types of companies, multiple types of business, and use cases.';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageView(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Rubén Guerrero - Senior Software Engineer" />
-        <meta property="og:description" content={description} />
-        <meta name="description" content={description} />
+        <title>Rubén Guerrero - Senior Software Engineer</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta property='og:title' content='Rubén Guerrero - Senior Software Engineer' />
+        <meta property='og:description' content={description} />
+        <meta name='description' content={description} />
       </Head>
-      <Component {...pageProps} />
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,32 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-import { GA_TRACKING_ID } from 'lib/gtag';
-
-const GASnippet = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '${GA_TRACKING_ID}', {
-  page_path: window.location.pathname,
-});
-`;
+import { Html, Head, Main, NextScript } from 'next/document';
 
 const MyDocument = () => {
   return (
     <Html lang="en">
-      <Head>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: GASnippet,
-          }}
-        />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;0,900;1,400&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head title="Rubén Guerrero - Senior Software Engineer" />
       <body>
         <Main />
         <NextScript />
