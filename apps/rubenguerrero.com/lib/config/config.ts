789bca5b9c5bf1a9b4f7147bf15014521ff6b78d
RubenGuerrero <ruben.guerrero@boopos.com>
2022-12-06 21:49:40 +0100

    fix: styles

--- apps/rubenguerrero.com/lib/config/config.ts
+++ apps/rubenguerrero.com/lib/config/config.ts
@@ -1,3 +1,4 @@
+console.log('RRR', process.env);
 const config = {
   TRACKING_HOST: process.env.NEXT_PUBLIC_TRACKING_HOST as string,
   TRACKING_KEY: process.env.NEXT_PUBLIC_TRACKING_KEY as string,
