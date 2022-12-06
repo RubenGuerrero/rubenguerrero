5cd845b79d8f5fce2b767a4929bcb455444cff39
RubenGuerrero <ruben.guerrero@boopos.com>
2022-12-06 22:09:55 +0100

    refactor: remove console

--- apps/rubenguerrero.com/lib/config/config.ts
+++ apps/rubenguerrero.com/lib/config/config.ts
@@ -1,4 +1,3 @@
-console.log('RRR', process.env);
 const config = {
   TRACKING_HOST: process.env.NEXT_PUBLIC_TRACKING_HOST as string,
   TRACKING_KEY: process.env.NEXT_PUBLIC_TRACKING_KEY as string,
