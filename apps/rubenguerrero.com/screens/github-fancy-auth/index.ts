9b0945f54cf6f6efcfa98fac29ed11f13f1c9d34
RubenGuerrero <ruben.guerrero@boopos.com>
2022-12-06 21:38:23 +0100

    fix: remove broker export

--- apps/rubenguerrero.com/screens/github-fancy-auth/index.ts
+++ apps/rubenguerrero.com/screens/github-fancy-auth/index.ts
@@ -1,5 +1,1 @@
 export * from './GithubFancyAuthScreen';
-export { ParentBox } from 'screens/github-fancy-auth/ParentBox';
-export { LoggedUser } from 'screens/github-fancy-auth/LoggedUser';
-export { LoggedUserProps } from 'screens/github-fancy-auth/LoggedUser';
-export { NotLoggedUser } from 'screens/github-fancy-auth/NotLoggedUser';
